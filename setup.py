1c213f234fe9daeb5d50a4578bf145dca8a3e9fe
jiawei397 <jwqd397>
2019-08-11 17:50:44 +0800

    feat: 模块说明

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name="jw_test",
-      version="1.0.2",
+      version="1.0.3",
       description="jw module",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author="jw",
       author_email="jw397@126.com",
       py_modules=["jw.__init__", "jw.test"])
